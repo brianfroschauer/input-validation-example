85065096aca5fee57b424c91d96c10544c5e35df
Brian Froschauer <bafroschauer@gmail.com>
2019-11-30 11:09:50 -0300

    refactor(Metric): fix indents

--- src/main/kotlin/com/example/inputvalidation/entity/Metric.kt
+++ src/main/kotlin/com/example/inputvalidation/entity/Metric.kt
@@ -5,15 +5,15 @@ import javax.persistence.*
 @Entity
 data class Metric(
 
-        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-        val id: Long = 0,
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
 
-        @Column(name = "name", nullable = false, unique = true)
-        val name: String = "",
+    @Column(name = "name", nullable = false, unique = true)
+    val name: String = "",
 
-        @Column(name = "samples", nullable = false)
-        var samples: Int = 0,
+    @Column(name = "samples", nullable = false)
+    var samples: Int = 0,
 
-        @Column(name = "time", nullable = false)
-        var time: Long = 0
+    @Column(name = "time", nullable = false)
+    var time: Long = 0
 )
